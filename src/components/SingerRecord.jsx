abcaaf4df194c6a21b6b36faddbe7bc26d2b760c
Frazer Chan <frazer@pop-os.localdomain>
2021-02-11 22:18:36 +0000

    Singer dob formatting

--- src/components/SingerRecord.jsx
+++ src/components/SingerRecord.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 function SingerRecord({ singer }) {
+  const formatDate = (date) => {
+    let year = date.substring(0, 4);
+    let month = date.substring(4, 6);
+    let day = date.substring(6, 8);
+
+    return [day, month, year].join("/");
+  };
+
   return (
     <div className="row border py-1 result-record">
       <div className={"col height-3em"}>singer</div>
       <div className={"col height-3em"}>{singer.name}</div>
       <div className={"col height-3em"}>{singer.sex}</div>
-      <div className={"col height-3em"}>{singer.dob}</div>
+      <div className={"col height-3em"}>{formatDate(singer.dob)}</div>
       <div className={"col-4 height-3em"}>{singer.company}</div>
     </div>
   );
