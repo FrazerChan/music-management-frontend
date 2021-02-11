4228c279971cdd43d85d0178fc600c7f27ebc8de
Frazer Chan <frazer@pop-os.localdomain>
2021-02-11 13:01:52 +0000

    Added axios for http requests.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import SingersList from "./components/SingersList";
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <SingersList />
     </div>
   );
 }
---
+++ src/components/SingersList.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios"
+
+function SingersList(props) {
+
+    const [singers, setSingers] = useState();
+
+    const getSingers = async()=>{
+        const response = await axios.get("http://localhost:8080/singers");
+
+        if (response.data){
+            setSingers(response.data);
+        }
+    }
+
+    useEffect(()=>{getSingers()},[])
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default SingersList;
